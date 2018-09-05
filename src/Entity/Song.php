c9e826f4b056097f9da0f5a2bf22dbd8a2a96a56
Temitope Olotin <temitope.olotin@andela.com>
2018-09-06 00:21:03 +0100

    create song entity and set the mapping

---
+++ src/Entity/Song.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SongRepository")
+ */
+class Song
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $length;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Album", inversedBy="songs")
+     */
+    private $Album;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getLength(): ?string
+    {
+        return $this->length;
+    }
+
+    public function setLength(string $length): self
+    {
+        $this->length = $length;
+
+        return $this;
+    }
+
+    public function getAlbum(): ?Album
+    {
+        return $this->Album;
+    }
+
+    public function setAlbum(?Album $Album): self
+    {
+        $this->Album = $Album;
+
+        return $this;
+    }
+}
