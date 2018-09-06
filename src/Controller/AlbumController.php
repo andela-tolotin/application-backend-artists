938184b094fca3c184063868a5e98c92b0a53bde
Temitope Olotin <temitope.olotin@andela.com>
2018-09-07 00:47:59 +0100

    modify normalizer to return a collection

--- src/Controller/AlbumController.php
+++ src/Controller/AlbumController.php
@@ -4,16 +4,52 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\AlbumRepository;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class AlbumController extends AbstractController
 {
+	use \App\Utils\JsonResponseTrait;
+
+	private $albumRepository;
+
+	public function __construct(AlbumRepository $albumRepository)
+	{
+		$this->albumRepository = $albumRepository;
+	}
+
     /**
      * @Route("/album", name="album")
      */
     public function index()
     {
-        return $this->render('album/index.html.twig', [
-            'controller_name' => 'AlbumController',
-        ]);
+        $albums = $this->albumRepository->findAll();
+
+		$encoder = new JsonEncoder();
+		$normalizer = new ObjectNormalizer();
+
+		$normalizer->setCircularReferenceHandler(function ($object) {
+			return $object->getTitle();
+		});
+
+		$serializer = new Serializer(array($normalizer), array($encoder));
+
+		$data = $serializer->normalize($albums, null, 
+			[
+				'attributes' => [
+					'token', 'title', 'cover', 'description', 
+					'songs' => [
+						'title', 'length'
+					],
+					'artist' => [
+						'name', 'token'
+					]
+				]
+			]
+		);
+
+		return $this->sendJsonResponse($serializer->serialize($data, 'json'), 200);
     }
 }
